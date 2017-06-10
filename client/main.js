d886bade6860e69f0358bc1dcad7b0e6b1c4fff9
robabby <robabby23@gmail.com>
2017-06-10 10:31:37 -0500

    Adding players from Mongo

--- client/main.js
+++ client/main.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-
-const players = [{
-  _id: '1',
-  name: 'Shayna',
-  score: 99
-}, {
-  _id: '2',
-  name: 'Brian',
-  score: -1
-}, {
-  _id: '3',
-  name: 'Rob',
-  score: 15
-}];
+import { Players } from './../imports/api/players';
+import { Tracker } from 'meteor/tracker';
 
 const renderPlayers = function(playersList) {
   return playersList.map(function(player) {
@@ -23,16 +11,20 @@ const renderPlayers = function(playersList) {
 };
 
 Meteor.startup(function() {
-  let name = 'Rob';
-  let title = 'foo';
-  let jsx = (
-    <div>
-      {/* Render h1 tag with text */}
-      <h1>{title}</h1>
-      <p>Hello {name}</p>
-      <p>This is a second p</p>
-      {renderPlayers(players)}
-    </div>
-  );
-  ReactDOM.render(jsx, document.getElementById('app'));
+  // Call Tracker.autorun
+  Tracker.autorun(function() {
+    let players = Players.find().fetch();
+
+    let jsx = (
+      <div>
+        {renderPlayers(players)}
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+
+  Players.insert({
+    name: 'Mark',
+    score: 15
+  });
 });
--- server/main.js
+++ server/main.js
@@ -2,9 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 
 Meteor.startup(function() {
-  Players.insert({
-    name: 'Shayna',
-    score: 5
-  });
   console.log(Players.find().fetch());
 });

ff4edced71ac52b6e26e2ab5e3622045c18d80af
robabby <robabby23@gmail.com>
2017-06-17 11:51:06 -0500

    Adding, removing players and Incrementing scores :+1:

--- client/main.js
+++ client/main.js
@@ -4,27 +4,61 @@ import { Meteor } from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 import { Tracker } from 'meteor/tracker';
 
-const renderPlayers = function(playersList) {
-  return playersList.map(function(player) {
-    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+const renderPlayers = (playersList) => {
+  return playersList.map((player) => {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s).
+        <button onClick={() => {
+          Players.update(player._id, {
+            $inc: {
+              score: 1
+            }
+          })
+        }}>+1</button>
+        <button onClick={() => {
+          Players.update(player._id, {
+            $inc: {
+              score: -1
+            }
+          })
+        }}>-1</button>
+        <button onClick={() => Players.remove(player._id)}>X</button>
+      </p>
+    );
   });
 };
 
-Meteor.startup(function() {
-  // Call Tracker.autorun
-  Tracker.autorun(function() {
-    let players = Players.find().fetch();
+const handleSubmit = (e) => {
+  let playerName = e.target.playerName.value;
+  e.preventDefault();
 
+  if (playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 0
+    });
+  }
+}
+
+Meteor.startup(() => {
+  // Call Tracker.autorun
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let name = 'Rob';
     let jsx = (
       <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
         {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name" />
+          <button>Add player</button>
+        </form>
       </div>
     );
     ReactDOM.render(jsx, document.getElementById('app'));
-  });
-
-  Players.insert({
-    name: 'Mark',
-    score: 15
   });
 });
